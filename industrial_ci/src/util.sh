c3931d885b0bfc2b36b168dc3ffe56842185f572
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2019-04-11 00:09:49 +0200

    print section name at the beginning of each fold

--- industrial_ci/src/util.sh
+++ industrial_ci/src/util.sh
@@ -67,6 +67,7 @@ function ici_time_start {
         echo -en "\e[0Kici_time:start:$ICI_TIME_ID"
     fi
     ici_color_output $ANSI_BLUE ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+    ici_color_output $ANSI_BLUE "Starting function '$ICI_FOLD_NAME'"
     if [ "$DEBUG_BASH" ] && [ "$DEBUG_BASH" == true ]; then set -x; fi
 }
 
@@ -98,7 +99,7 @@ function ici_time_end {
         echo -en "ici_fold:end:$name"
     fi
     ici_color_output "$color_wrap" "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-    echo -e "\e[0K\e[${color_wrap}mFunction $name returned with code '${exit_code}' after $(( elapsed_seconds / 60 )) min $(( elapsed_seconds % 60 )) sec \e[0m\n"
+    echo -e "\e[0K\e[${color_wrap}mFunction '$name' returned with code '${exit_code}' after $(( elapsed_seconds / 60 )) min $(( elapsed_seconds % 60 )) sec \e[0m\n"
 
     unset ICI_FOLD_NAME
     if [ "$DEBUG_BASH" ] && [ "$DEBUG_BASH" == true ]; then set -x; fi

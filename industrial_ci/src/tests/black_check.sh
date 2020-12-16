c3afa48fba64fc78a6310c9737d0036c8e81ab49
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2020-12-16 18:01:27 +0100

    migrate to components
    * builders, folding, tests, isolation
    * shell isolation
    * no unbound variables (bash 4.4+)

--- industrial_ci/src/tests/black_check.sh
+++ industrial_ci/src/tests/black_check.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-function run_black_check() {
-  DOCKER_IMAGE=${DOCKER_IMAGE:-ros:${ROS_DISTRO}-ros-base} ici_require_run_in_docker # this script must be run in docker
+function prepare_black_check() {
+  DOCKER_IMAGE=${DOCKER_IMAGE:-ros:${ROS_DISTRO}-ros-base}  
+}
 
+function run_black_check() {
   if ! command -v "black" > /dev/null; then
     ici_time_start install_black
     ici_quiet ici_install_pkgs_for_command pip3 python3-pip python3-setuptools python3-wheel

1302aba1d891d3ae948b14272d32a89366eb3552
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2021-09-21 22:20:26 +0200

    copy9

--- industrial_ci/src/isolation/docker.sh
+++ industrial_ci/src/isolation/docker.sh
@@ -154,8 +154,7 @@ function ici_run_cmd_in_docker() {
     docker_cp "$d" "$cid:${docker_query[*]:2}/" "${docker_query[0]}" "${docker_query[1]}"
   done
 
-  docker cp "$ICI_SRC_PATH" "$cid:$ICI_SRC_PATH/"
-
+  tar -c -f - "$ICI_SRC_PATH" | docker cp - "$cid:/"
 
   trap '>/dev/null ici_label ici_quiet docker kill --signal=SIGTERM $cid && >/dev/null docker wait $cid' INT
   ( trap '' INT &&  ici_label docker start -a "$cid" > >(sed 's/\r$//') ) &

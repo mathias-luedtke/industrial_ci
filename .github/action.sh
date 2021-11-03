9262622c23c157ab66e5ea97dd8ae6d096bcc693
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2021-11-03 23:27:52 +0100

    Full tests

--- .github/action.sh
+++ .github/action.sh
@@ -19,5 +19,27 @@
 
 # 2016/05/18 http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 
-echo "string"
-echo "::set-output name=answer::42"
+set -euo pipefail
+
+DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export TARGET_REPO_PATH=$GITHUB_WORKSPACE
+export TARGET_REPO_NAME=${GITHUB_REPOSITORY##*/}
+export _FOLDING_TYPE=github_actions
+
+ICI_SRC_PATH="$DIR_THIS/../industrial_ci/src"
+source "$ICI_SRC_PATH/util.sh"
+
+if [ -n "${INPUT_CONFIG-}" ]; then
+    ici_exec_for_command jq ici_error "In order to use the config parameter, please install jq"
+    vars=$(jq -r 'keys[] as $k | "export \($k)=\(.[$k]|tojson)" | gsub("\\$\\$";"\\$")' <<< "$INPUT_CONFIG"  | grep "^export [A-Z][A-Z_]*=")
+    echo "$vars"
+    eval "$vars"
+fi
+
+if [ "${ABICHECK_MERGE:-}" = "auto" ]; then
+  export ABICHECK_MERGE=false
+  [ "$GITHUB_EVENT_NAME" != "pull_request" ] || ABICHECK_MERGE=true
+fi
+
+env "$@" bash "$DIR_THIS/../industrial_ci/src/ci_main.sh"

96b5746a8c957ff0e201af5811d5340d0477b3bc
Mathias Lüdtke <727227+mathias-luedtke@users.noreply.github.com>
2022-10-12 22:11:56 +0200

    process env

--- .github/action.js
+++ .github/action.js
@@ -1,5 +1,6 @@
 'use strict';
 var {spawnSync} = require('child_process');
+console.log(process.env);
 var r = spawnSync(__dirname + '/action.sh', { stdio: 'inherit'});
 if (r.error) {
   throw r.error;
